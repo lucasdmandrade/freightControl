b764d48ca6da11c74f429f4af14a12d797ab59a7
Lucas Andrade <lucasdmandrade@gmail.com>
2023-03-28 21:17:04 -0300

    feat(login) create login page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["tailwindui.com/img/logos/mark.svg"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html className="h-full bg-gray-50" lang="pt-br">
       <Head />
-      <body>
+      <body className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
